764eba5f6a206f2b6526950128c4ab4d3a722562
horimori <hori.was.taken@gmail.com>
2015-12-06 12:09:37 -0500

    Update gd_push.py
    This is essentially it

--- cmds/gd_push.py
+++ cmds/gd_push.py
@@ -1,11 +1,14 @@
-import dropbox
-import os
-key = open("user_key","r")
-data = key.read()
-client = dropbox.client.DropboxClient(data)
+import dropbox, os
+from . import gd_utilities as gd_util
+access_key = gd_util.get_key('user_key')
+if access_key == None:
+    print("User access key was not found, make sure it is properly configured")
+client = dropbox.client.DropboxClient(access_key)
 #basic outline
 #iterate thrrough the loaded doirectory
-upload = os.listdir("/loaded")
+#upload = os.listdir("/loaded")
+upload = os.listdir(os.path.dirname(os.path.abspath(__file__)))
 for file in upload:
-    j = open(file)
+    f = open(file)
     rep = client.put_file(j)
+    f.close()
