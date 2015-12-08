5c0bc3e3e83a90e3fd462d8c061723d9a6acbc01
Rui Zhang <r.zhang194@gmail.com>
2015-12-08 00:15:45 -0500

    Modifies gd_push.py and is now able to push

--- cmds/gd_push.py
+++ cmds/gd_push.py
@@ -1,15 +1,33 @@
-import dropbox, os
+import dropbox, os, sys
 from . import gd_utilities as gd_utils
-def push():
+def push(cmds):
     access_key = gd_utils.get_key('user_key')
     if access_key == None:
         print("User access key was not found, make sure it is properly configured")
+        sys.exit(0)
     client = dropbox.client.DropboxClient(access_key)
     #basic outline
     #iterate thrrough the loaded doirectory
     #upload = os.listdir("/loaded")
-    upload = os.listdir(os.path.dirname(os.path.abspath(__file__)))
+    ld_folder, dst = None, None
+    if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded')) == False:
+        ld_folder = os.mkdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded'))
+    else:
+        ld_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded')
+
+    upload = os.listdir(ld_folder)
+    if len(upload) == 0:
+        print("Nothing to upload")
+        sys.exit('/')
+
+    if len(cmds) == 0:
+        dst = '/'
+        print("No destination specified, defaulting to root \'/\'")
+    else:
+        dst = cmds[0]
+
     for file in upload:
-        f = open(file)
-        rep = client.put_file(f)
+        f = open(os.path.join(ld_folder, file))
+        rep = client.put_file(dst + file, f, overwrite=True)
+        print(rep['path'])
         f.close()
