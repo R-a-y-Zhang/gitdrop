e05393a64b2987c54ee228183266004f642c84bd
Rui Zhang <r.zhang194@gmail.com>
2016-05-13 23:32:39 -0400

    Adds file tracking and untracking

--- cmds/gd_track.py
+++ cmds/gd_track.py
@@ -1,127 +1,86 @@
-import os, datetime, sys, shutil
+import os, sys, shutil, path
+from . import gd_utilities a gd_utils
+	
+def rewrite_file(data):
+	log_file = gd_utils.get_key('files_log')
+	with open(log_file, 'w') as f:
+		for d in data:
+			f.write(d)
 
-def track(args):
-    untrack = False
-    if args[0] == '--u':
-        untrack = True
-    if os.path.isfile('tracked_files.txt')==False:
-        tracker_file = open('tracked_files.txt', 'a')
-    tracker_file = open('tracked_files.txt', 'r')
-    status = tracker_file.readlines()
-    tracker_file.close()
-    files_tracked, modifications = [line.split(": ")[0] for line in status], \
-                                    [line.split(": ")[1] for line in status]
-    print (args[0:])
-    fs_list = args[0:]
-    print (fs_list)
-    for f in fs_list:
-        abs_path = check_in_dir(f)
+def track_file(filepath):
+	log_file = gd_utils.get_key('files_log')
+	with open(log_file, 'a') as f:
+		time_mod = os.path.getmtime(filepath)	
+		size_mod = os.path.getsize(filepath)
+		path_mod = os.path.abspath(filepath)
+		f.write('{}-{}-{}-{}-{}\n'.format(path_mod, time_mod, time_mod, size_mod, size_mod))
 
-        if os.path.exists(abs_path) == False: # if file does not exist
-            print("File: %s not found" % (abs_path))
-            continue
+def untrack_files(files_list):
+	log_file = gd_utils.get_key('files_log')
+	files = []
+	with open(log_file) as f:
+		files = f.readlines()
+	
+	updated_list = []
+	for file in files:
+		if file.split(' ')[0] not in files:
+			updated_list.append(file)
+	
+	rewrite_file(updated_list)
 
-        mod_date = datetime.datetime.fromtimestamp(os.stat(abs_path).st_mtime)
-        time_str = "{}-{}-{} {}:{}:{}".format(mod_date.day, \
-            mod_date.month, mod_date.year, mod_date.hour, mod_date.minute, mod_date.second)
+def update_times():
+	log_file = gd_utils.get_key('files_log')
+	files = []
+	with open(log_file) as f:
+		files = f.readlines()
+	
+	for i in range(len(files)):
+		f_data = f.split('-')
+		time_mod = os.path.getmtime(files[i])
+		size_mod = os.path.getsize(files[i])
+		files[i] = '{}-{}-{}-{}-{}\n'.format(f_data[0], f_data[1], time_mod, f_data[3], size_mod)
+	
+	rewrite_file(files)
 
-        if abs_path not in files_tracked: # if not already tracked, track it
-            status.append("{}: {}".format(abs_path, time_str))
-        if abs_path in files_tracked: # if is tracked, update time
-            target = "{}: {}".format(abs_path, time_str)
-            status[files_tracked.index(abs_path)] = target
+def update_push_times():
+	log_file = gd_utils.get_key('files_log')
+	files = []
+	with open(log_file) as f:
+		files = f.readlines()
 
-    tracker_file = open('tracked_files.txt', 'w')
-    write_to_tracked(status)
+	for i in range(len(files)):
+		f_data = files[i]
+		time_mod = os.path.getmtime(f_data[0])
+		size_mod = os.path.getsize(f_data[0])
+		files[i] = '{}-{}-{}-{}-{}\n'.format(f_data[0], time_mod, time_mod, size_mod, size_mod)
+		
+	rewrite_file(files)
 
-def status(args):
-    if args[0] == '--loaded':
-        pass
+def get_status():
+	log_file = gd_utils.get_key('files_log')
+	files = []
+	with open(log_file) as f:
+		files = f.readlines()
+	
+	out_lens = [0, 0, 0, 0, 0]
+	fnames = []
+	f_push_times = []
+	f_update_times = []
+	f_push_size = []
+	f_update_size = []
+	with file in files:
+		f_data = file.split('-')
+		fnames.append(f_data[0])
+		f_push_times(time.ctime(f_data[1]))
+		f_update_times(time.ctime(f_data[2]))
+		f_push_size(f_data[3])
+		f_update_size(f_data[4])
+		for i in range(len(f_data)):
+			if (len(f_data[i]) > out_lens[i]):
+				out_lens[i] = len(f_data[i])
 
-    else:
-        fs_list = args
-        tracker_file = open('tracked_files.txt', 'r')
-        status = tracker_file.readlines()
-        tracker_file.close()
-        files_tracked, mod_times = [line.split(": ")[0] for line in status], \
-                                    [line.split(": ")[0] for line in status]
-        for f in fs_list:
-            abs_path = check_in_dir(f)
+	str_output = '{0:%d}   {1:%d}   {2:%d}   {3:%d}   {4:%d}\n' % (out_lens[0], out_lens[1], out_lens[2], out_lens[3], out_lens[4])
+	
+	for i in range(len(fnames)):
+		str_output.format(fnames[i], f_push_times[i], f_update_times[i], f_push_size[i], f_update_size[i])
 
-            if os.path.exists(abs_path) == False:
-                print("File: %s not found" % (abs_path))
-                continue
-
-            if abs_path not in files_tracked:
-                print("File: %s not tracked" % (abs_path))
-
-            elif abs_path in files_tracked:
-                ltm = datetime.datetime.fromtimestamp(os.stat(abs_path).st_mtime)
-                formatted_time = "{}-{}-{} {}:{}:{}".format(ltm.day, ltm.month,\
-                ltm.year, ltm.hour, ltm.minute, ltm.second)
-                if ltm == mod_times[files_tracked.index(abs_path)]:
-                    print("File %s: No change since last loaded" % (abs_path))
-
-                if ltm == modtimes[files_tracked.index(abs_path)]:
-                    print("File %s: Changed since last loaded" % (abs_path))
-
-        output = open('tracked_files.txt', 'w')
-        write_to_tracked(status)
-
-
-def load(args):
-    tracker_file = open('tracked_files.txt', 'r')
-
-
-
-    status = tracker_file.readlines()
-
-
-
-    tracker_file.close()
-    files_tracked, modifications = [line.split(": ")[0] for line in status], \
-                                    [line.split(": ")[1] for line in status]
-    if os.path.isdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded')) == False:
-        os.mkdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded'))
-    print (files_tracked)
-    if len (args)==0:
-
-        for f in files_tracked:
-            src = check_in_dir(f)
-            dst = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded')
-            shutil.copy(src,dst)
-    for f in args:
-        abs_path = check_in_dir(f)
-        if os.path.exists(abs_path) == False:
-            print("File: %s not found" % (abs_path))
-            continue
-
-        src = abs_path
-        dst = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'loaded')
-        print (src)
-        print (dst)
-        shutil.copy(src, dst)
-        ltm = datetime.datetime.fromtimestamp(os.stat(abs_path).st_mtime)
-        formatted_time = "{}-{}-{} {}:{}:{}".format(ltm.day, ltm.month, \
-            ltm.year, ltm.hour, ltm.minute, ltm.second)
-        if src not in files_tracked:
-            status.append("{}: {}".format(src, formatted_time))
-        if src in files_tracked:
-            status[files_tracked.index(src)] = "{}: {}".format(src, formatted_time)
-
-
-
-
-def check_in_dir(f):
-    if os.path.exists(os.path.join(os.getcwd(), f)) == True:
-        return os.path.join(os.getcwd(), f)
-    return f
-
-def write_to_tracked(status):
-    if len(status) != 0:
-        output = open('tracked_files.txt', 'w')
-        for s in status:
-            print (s)
-            output.write(s + '\n')
-
-        output.close()
